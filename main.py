a0fbd196ae4d1818e72205366e7ef52e6251c5af
Note Soumu <soumunote@gmail.com>
2020-08-16 17:30:21 +0900

    modify hard PWM

--- main.py
+++ main.py
@@ -2,13 +2,16 @@ import RPi.GPIO as GPIO
 import time
 
 INTERVAL = 0.15
-PIN = 14
 FREQ = 50
 
+GPIO.cleanup()
 GPIO.setmode(GPIO.BCM)
 
-GPIO.setup(PIN, GPIO.OUT)
-servo = GPIO.PWM(PIN, FREQ)
+GPIO.setup(15, GPIO.OUT)
+GPIO.output(15, GPIO.HIGH)
+
+GPIO.setup(18, GPIO.OUT)
+servo = GPIO.PWM(18, FREQ)
 
 servo.start(0.0)
 
@@ -19,6 +22,5 @@ for i in range(10):
     servo.ChangeDutyCycle(10)
     time.sleep(INTERVAL)
 
+
 GPIO.cleanup()
-
-
