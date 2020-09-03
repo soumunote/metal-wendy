43fc3c52f70690e9d07d5c15f9e31d3be5b69d28
Note Soumu <soumunote@gmail.com>
2020-09-03 23:00:33 +0900

    ears move

--- samples/wings_action.py
+++ samples/wings_action.py
@@ -5,12 +5,20 @@ import time
 
 pi = pigpio.pi()
 pi.set_mode(18, pigpio.OUTPUT)
+pi.set_mode(19, pigpio.OUTPUT)
 
-for i in range(1, 3):
-  pi.hardware_PWM(18, 50, 25000)
-  time.sleep(.2)
+pi.hardware_PWM(19, 50,120000)
+
+for i in range(1, 6):
+  pi.hardware_PWM(18, 50, 72500)
+  pi.hardware_PWM(19, 50, 72500)
+  #pi.hardware_PWM(18, 50, 25000)
+  #pi.hardware_PWM(19, 50,120000)
+  time.sleep(0.3)
   pi.hardware_PWM(18, 50,120000)
-  time.sleep(0.2)
+  pi.hardware_PWM(19, 50, 25000)
+  time.sleep(1)
 
 pi.set_mode(18, pigpio.INPUT)
+pi.set_mode(12, pigpio.INPUT)
 pi.stop()
